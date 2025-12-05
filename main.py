c5f93e21b3e6a174a9d7557621f2e090640b95b8
Tassio Lopes <tassio.lopes05@gmail.com>
2025-12-05 09:42:49 -0300

    versao 1
    apaguei tudo

--- main.py
+++ main.py
@@ -2,32 +2,7 @@ import flet as ft
 from screenManager import Navegation
 from time import sleep
 def main(page: ft.Page):
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.window.width = 1366
-    page.window.height = 768
-    page.update()
-
-    page.window.maximized = True
-    page.update()
-    navegation = Navegation(page)
-
-    page.on_route_change = navegation.change_screen
-
-    page.go('bemvindo')
-    page.data = {"tipo_processo":''}
-
-    page.update()
-    sleep(1/120)
-    bemvindo = 'xercícios Anteriores'
-    for letra in bemvindo:
-        page.views[-1].controls[-1].content.controls[-1].value += letra
-        sleep(1/15)
-        page.update()
-    sleep(1 / 15)
-    page.go('/')
-    page.update()
-
-
+    pass
 
 
 
